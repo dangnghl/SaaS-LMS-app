49991a96aa954dcaa66ec08c59cc7db268bf6895
Nghi <Nguyendangnghi2002@gmail.com>
2025-08-01 17:45:13 -0700

    first commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewComponions = () => {
+  return (
+    <div>NewComponions</div>
+  )
+}
+
+export default NewComponions
---
+++ app/companions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComponionsLibrary = () => {
+  return (
+    <div>ComponionsLibrary</div>
+  )
+}
+
+export default ComponionsLibrary
